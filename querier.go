38908bdf4c3345f9191bd62fe642eecd9be79e93
Diego Borges <drborges.cic@gmail.com>
2015-06-05 09:21:11 -0300

    Adds API documentation

--- querier.go
+++ querier.go
@@ -5,11 +5,21 @@ import (
 	"reflect"
 )
 
+// Querier provides hight level query operations
 type Querier struct {
 	c appengine.Context
 	q *Query
 }
 
+// All loads all matched items into the given slice
+//
+// It supports up to 1000 results sinces it delegates
+// the job to datastore.Query.GetAll which imposes
+// this limitation
+//
+// Future implementation will work with channels
+// to overcome this limitation and let the users
+// themselves dictate this limitation
 func (this Querier) All(slice interface{}) error {
 	keys, err := this.q.GetAll(this.c, slice)
 
@@ -24,6 +34,11 @@ func (this Querier) All(slice interface{}) error {
 	return nil
 }
 
+// First loads the first matched item into the
+// given entity
+//
+// datastore.Done is returned if there is no
+// matched item
 func (this Querier) First(entity entity) error {
 	i := this.q.Run(this.c)
 	key, err := i.Next(entity)
@@ -35,6 +50,11 @@ func (this Querier) First(entity entity) error {
 	return nil
 }
 
+// EntityAt retrieves the entity type at position i
+// in the given slice
+//
+// It panics in case the slice parameter is not either
+// a slice or a slice pointer
 func EntityAt(slice interface{}, i int) entity {
 	s := reflect.ValueOf(slice)
 
@@ -42,7 +62,7 @@ func EntityAt(slice interface{}, i int) entity {
 		return s.Index(i).Interface().(entity)
 	}
 
-	if s.Type().Elem().Kind() == reflect.Slice {
+	if s.Kind() == reflect.Ptr && s.Type().Elem().Kind() == reflect.Slice {
 		return s.Elem().Index(i).Interface().(entity)
 	}
 
